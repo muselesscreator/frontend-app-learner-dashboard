cb6ecb4b9927d4fcaaec5518a24d66f0c370eaaf
Leangseu Kim <lkim@edx.org>
2022-05-23 15:22:35 -0400

    add basic ui

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -5,13 +5,21 @@ import WidgetSidebar from 'containers/WidgetSidebar';
 import { courseIDs } from 'data/services/lms/fakeData/courses';
 import EmptyCourse from '../EmptyCourse';
 
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
+import messages from '../Dashboard/messages';
+
 export const Dashboard = () => {
   return (
-    <div className='d-flex p-2'>
+    <div className='d-flex flex-column p-2'>
       {courseIDs.length ? (
         <>
-          <CourseList courseIDs={courseIDs} />
-          <WidgetSidebar />
+          <h2 className='py-2'>
+            <FormattedMessage {...messages.myCourse} />
+          </h2>
+          <div className='d-flex'>
+            <CourseList courseIDs={courseIDs} />
+            <WidgetSidebar />
+          </div>
         </>
       ) : (
         <EmptyCourse />
